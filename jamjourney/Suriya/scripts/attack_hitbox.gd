03c88e67c024f52c27b58c073da0416e7736f5e4
SuriyaV-123 <suriyavelayudham@gmail.com>
2025-11-22 12:54:31 +0000

    Now got animation for a basic attack

--- jamjourney/Suriya/scripts/attack_hitbox.gd
+++ jamjourney/Suriya/scripts/attack_hitbox.gd
@@ -1,17 +1,11 @@
 extends Area2D
 
+
 var atk = 10
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _init() -> void:
-	collision_layer = 1
-	collision_mask = 2
-
-func attack() -> void:
-	if Input.is_action_just_pressed("right_click"):
-		pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	collision_layer = 2
+	collision_mask = 1
--- jamjourney/Suriya/scripts/vampire.gd
+++ jamjourney/Suriya/scripts/vampire.gd
@@ -2,20 +2,31 @@ extends CharacterBody2D
 
 var speed = 300
 var health = 100
-var atk = 10
 
 var click_position = Vector2()
 var target_position = Vector2()
 
 @onready var timer: Timer = $Timer
+@onready var sprite: AnimatedSprite2D = $BasicAttack
+@onready var hitbox: Area2D = $AttackHitbox
+@onready var hitbox_shape: CollisionShape2D = $AttackHitbox/CollisionShape2D
 
-
+var attacking: bool = false
 
 func _ready():
 	click_position = position
 	die()
 
+func _process(delta):
+	if Input.is_action_just_pressed("button_1") and not attacking:
+		start_attack()
 
+func start_attack():
+	attacking = true
+	hitbox_shape.disabled = false
+	sprite.play("default")
+	_on_basic_attack_animation_finished()
+	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("left_click"):
 		click_position = get_global_mouse_position()
@@ -32,3 +43,9 @@ func die():
 func _on_timer_timeout() -> void:
 	print("dead")
 	queue_free()
+
+
+func _on_basic_attack_animation_finished() -> void:
+	if sprite.animation == "default":
+		hitbox_shape.disabled = true
+		attacking = false
