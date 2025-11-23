c68abda293ef758c78dee9827175653abe13ffb8
SuriyaV-123 <suriyavelayudham@gmail.com>
2025-11-23 00:50:56 +0000

    THE LICH IS GOING TO COMMIT ARSON

--- jamjourney/Suriya/scripts/lich.gd
+++ jamjourney/Suriya/scripts/lich.gd
@@ -3,20 +3,30 @@ extends CharacterBody2D
 var speed = 300
 var damage = 50
 
+@onready var fireball = load("res://Suriya/Scenes/fireball.tscn")
+
 @onready var health_module: Health = $HealthModule
+#variables for hitboxes and stuff
 @onready var sprite: AnimatedSprite2D = $pivot/BasicAttack
 @onready var hitbox: Area2D = $pivot/AttackHitbox
 @onready var hitbox_shape: CollisionShape2D = $pivot/AttackHitbox/CollisionShape2D
 @onready var pivot: Node2D = $pivot
 
+@onready var root = get_tree().get_root().get_node("LichBody2D")
+#different timers
 @onready var death_timer: Timer = $death_timer
+@onready var teleport_cd: Timer = $teleport_cd
+@onready var fireball_cd: Timer = $fireball_cd
 
 @export var teleport_range : float = 300
+@export var fireball_range : float = 1000
 
 var click_position = Vector2()
 var target_position = Vector2()
 
 var attacking = false
+var teleport_ready = true
+var fireball_ready = true
 
 func _ready():
 	click_position = position
@@ -27,8 +37,11 @@ func _process(_delta):
 	pivot.rotation = global_position.direction_to(get_global_mouse_position()).angle() - PI
 	if Input.is_action_just_pressed("button_1") and not attacking:
 		start_attack()
-	if Input.is_action_just_pressed("button_2") and not attacking:
+	elif Input.is_action_just_pressed("button_2") and not attacking and teleport_ready:
 		start_teleport()
+	elif Input.is_action_just_pressed("button_3") and not attacking and fireball_ready:
+		start_shoot()
+	
 	
 	
 func _physics_process(_delta):
@@ -48,11 +61,25 @@ func start_attack():
 
 func start_teleport():
 	attacking = true
+	teleport_ready = false
+	teleport_cd.start()
 	if global_position.distance_to(get_global_mouse_position()) <= teleport_range:
 		global_position = get_global_mouse_position()
 		click_position = get_global_mouse_position()
 		
 	attacking = false
+
+func start_shoot():
+	var fireball_instance = fireball.instantiate()
+	attacking = true
+	fireball_ready = false
+	fireball_cd.start()
+	fireball_instance.dir = global_rotation
+	fireball_instance.spawn_pos = global_position
+	fireball_instance.spawn_rot = global_rotation
+	root.call_deferred("add_child",fireball_instance)
+	attacking = false
+	
 	
 
 func health_changed(_old, new):
@@ -75,3 +102,9 @@ func _on_Hitbox_body_entered(body):
 func _on_death_timer_timeout() -> void:
 	print("dead")
 	queue_free()
+	
+func _on_teleport_cd_timeout() -> void:
+	teleport_ready = true
+
+func _on_fireball_cd_timeout() -> void:
+	fireball_ready = true
--- jamjourney/fireball.gd
+++ jamjourney/fireball.gd
@@ -4,12 +4,13 @@ var dir : float
 var spawn_pos : Vector2
 var spawn_rot : float
 @export var damage = 100
-@export var speed = 0
+@export var speed = 600
 @onready var hitbox : Area2D = $Hitbox
 @onready var body : CollisionShape2D = $Hitbox/CollisionShape2D
 @onready var explode_anim : AnimatedSprite2D = $Explosion/ExplosionAnimation
 @onready var explode_body : CollisionShape2D = $Explosion/CollisionShape2D
 @onready var explode_area : Area2D = $Explosion
+@onready var despawn_timer : Timer = $despawn_timer
 
 
 
@@ -18,6 +19,7 @@ func _ready() -> void:
 	global_rotation = spawn_rot
 	hitbox.body_entered.connect(_on_Hitbox_body_entered)
 	explode_body.disabled = true
+	despawn_timer.start()
 	
 func _physics_process(_delta: float) -> void:
 	velocity = Vector2(0,-speed).rotated(dir)
@@ -43,6 +45,5 @@ func _on_explode_anim_finished():
 func _on_explode_Hitbox_entered(body):
 	Health.findHealthModule(body).dealDamage(damage)
 
-	
-	
-	
+func _on_despawn_timer_timeout() -> void:
+	queue_free()
